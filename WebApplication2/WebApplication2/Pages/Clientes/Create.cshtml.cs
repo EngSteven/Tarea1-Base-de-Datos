be0b2a6faf3282e89b89a995570a77ee00245a6f
Steven Sequeira <stesequeira11@gmail.com>
2023-08-27 10:24:38 -0600

    página mensaje inserción éxitosa

--- WebApplication2/WebApplication2/Pages/Clientes/Create.cshtml.cs
+++ WebApplication2/WebApplication2/Pages/Clientes/Create.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Data;
 using System.Data.SqlClient;
 using System.Data.SqlTypes;
+using System.Xml.Linq;
 
 namespace WebApplication2.Pages.Clientes
 {
@@ -17,26 +18,30 @@ namespace WebApplication2.Pages.Clientes
         }
 
         public void OnPost() 
-        { 
+        {
+
+
             clienteInfo.Nombre = Request.Form["Nombre"];
             clienteInfo.Precio = Request.Form["Precio"];
 
             
-            //comprobar que no este vacio los datos ingresados
+
             if (clienteInfo.Nombre.Length == 0 || clienteInfo.Precio.Length == 0 ) 
             {
                 errorMessage = "Todos los datos son requeridos.";
                 return;
             }
+            //Guardar el nuevo cliente
+            //Comprobar el formato. 
 
             //Comprobar que el nombre solo contenga letras o guines
-            if(!clienteInfo.Nombre.All(c => (Char.IsLetter(c) || c == '-')))
+            if (!clienteInfo.Nombre.All(c => (Char.IsLetter(c) || c == '-')))
             {
                 errorMessage = "El nombre solo puede contener letras o guines";
                 return;
             }
             //Comprobar que el precio solo contenga numeros o comas
-            if(!clienteInfo.Precio.All(c => (c >= '0' && c <= '9') || c == ','))
+            if (!clienteInfo.Precio.All(c => (c >= '0' && c <= '9') || c == ','))
             {
                 errorMessage = "El precio solo puede tener valores numéricos o coma";
                 return;
@@ -46,18 +51,15 @@ namespace WebApplication2.Pages.Clientes
             {
                 String connectionString = "Data Source=project0-server.database.windows.net;Initial Catalog=project0-database;Persist Security Info=True;User ID=stevensql;Password=Killua36911-";
                 string spName = "dbo.CrearArticulo";
-                using (SqlConnection connection = new SqlConnection(connectionString)) 
+                using (SqlConnection connection = new SqlConnection(connectionString))
                 {
                     connection.Open();
-                    /*String sql = "INSERT INTO Articulo " +
-                                 "(Nombre, Precio) VALUES " +
-                                 "(@Nombre, @Precio);";*/
                     using (SqlCommand command = new SqlCommand(spName, connection))
                     {
                         command.CommandType = CommandType.StoredProcedure;
 
                         command.Parameters.AddWithValue("@inNombre", clienteInfo.Nombre);
-                        command.Parameters.AddWithValue("@inPrecio", SqlMoney.Parse(clienteInfo.Precio));                        
+                        command.Parameters.AddWithValue("@inPrecio", SqlMoney.Parse(clienteInfo.Precio));
                         SqlParameter resultCodeParam = new SqlParameter("@outResultCode", SqlDbType.Int);
                         resultCodeParam.Direction = ParameterDirection.Output;
                         command.Parameters.Add(resultCodeParam);
@@ -65,16 +67,15 @@ namespace WebApplication2.Pages.Clientes
                         command.ExecuteNonQuery();
 
                         int resultCode = (int)command.Parameters["@outResultCode"].Value;
-                        
+
                         if (resultCode == 50001) //codigo generado en el SP que dice si ya un nombre del articulo existe o no
                         {
                             errorMessage = "El nombre del articulo ya existe";
                             return;
                         }
-
-
                     }
                 }
+
             }
             catch (Exception ex)
             {
@@ -85,7 +86,8 @@ namespace WebApplication2.Pages.Clientes
             clienteInfo.Precio = "";
             successMessage = "Nuevo articulo añadido correctamente.";
 
-            //Response.Redirect("/Index");
+            Response.Redirect("/Exito");
+
         }
     }
 }
